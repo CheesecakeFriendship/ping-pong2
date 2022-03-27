3b8672cd199714eb0294cf7467ede0d19bedafb3
Павел Соколов <pashock227@gmail.com>
2022-03-27 19:39:51 +0300

    я пересобрал всё, но пока программа не работает

--- Ping-pong.py
+++ Ping-pong.py
@@ -3,79 +3,76 @@ from pygame import *
 from random import randint
 
 class GameSprite(sprite.Sprite):
-#     def __init__(self, player_image, player_x, player_y, player_speed):
-#         super().__init__()
-#         self.image = transform.scale(image.load(player_image),(65, 65))
-#         self.speed = player_speed
-#         self.rect = self.image.get_rect()
-#         self.rect.x = player_x
-#         self.rect.y = player_y
-#     def reset(self):
-#         window.blit(self.image, (self.rect.x, self.rect.y))
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image),(65, 65))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player1(GameSprite):
-#     def update(self):
-#         keys = key.get_pressed()
-#         if keys[K_LEFT] and self.rect.x > 5:
-#             self.rect.x -= self.speed
-#         if keys[K_RIGHT] and self.rect.x < win_width - 80:
-#             self.rect.x += self.speed
-#         if keys[K_a] and self.rect.x > 5:
-#             self.rect.x -= self.speed
-#         if keys[K_d] and self.rect.x < win_width - 80:
-#             self.rect.x += self.speed
-#     def fire(self):
-#         keys = key.get_pressed()
-#         if keys[K_SPACE]:
-#             bullet = Bullet('bullet.png', self.rect.x, self.rect.top, 10)
-#             bullets.add(bullet)
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.x -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.x += self.speed
 
 class Player2(GameSprite):
-#     def update(self):
-#         self.rect.y += self.speed
-#         global lost
-#         if self.rect.y > win_height:
-#             self.rect.x = randint(0, win_height - 100)
-#             self.rect.y = 0
-#             lost = lost + 1
+    def update(self):
+        if keys[K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_d] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.x -= self.speed
+        if keys[K_s] and self.rect.y < win_width - 80:
+            self.rect.x += self.speed
         
-# window = display.set_mode((700, 500))
-# win_width = 700
-# win_height = 500
+window = display.set_mode((700, 500))
+win_width = 700
+win_height = 500
 # display.set_caption()
-# background = transform.scale(image.load(""),(700, 500))
-# sprite1 = Player("rocket.png", 10, 425, 5)
+# background = transform.scale(image.load(" "),(700, 500))
 
-# font.init()
-# font1 = font.SysFont('Arial', 36)
-# lost = 0
-
-# font.init()
-# font = font.SysFont('Arial', 70)
-# win = font.render('YOU WIN!', True, (255, 215, 0))
+font.init()
 
 font1 = font.Font(None, 35)
 lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
 
+font2 = font.Font(None, 35)
+lose2 = font2.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 
-# game = True
-# finish = False
+game = True
+finish = False
 while game:
-#     for e in event.get():
-#         if e.type == QUIT:
-#             game = False
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
 
     if finish != True:
         ball.rect.x += speed_x
         ball.rect.y += speed_y
         
-    if ball.rect.y > win_height-50
-        or ball.rect.y < 0:
+    if ball.rect.y > win_height-50 or ball.rect.y < 0:
             speed_y *= -1
-        
-#         sprite_list = sprite.spritecollide(sprite1, monsters, False)
-#         sprite_list = sprite.spritecollide(sprite1, asteroids, True)
-#         if sprite.groupcollide(monsters, bullets, True, True):
-#             monsters.add(Enemy("ufo.png", randint(1, 635), 1, randint(1, 2)))
+
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 200))
+
+    if ball.rect.x > 700:
+        finish = True
+        window.blit(lose2, (200, 200))
+
+    if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
 
     display.update()
